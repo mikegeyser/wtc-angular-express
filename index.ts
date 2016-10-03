21040258ddece5caf3a6bb470fba1b02ca182a8e
Mike Geyser <michaelg@bbd.co.za>
2016-10-03 11:34:26 +0200

    1
    Served up the static index.html page.

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 let express = require("express");
 let app = express();
+let path = require("path");
 
-app.get("/", (req, res) => res.send("Hello world!"));
+app.use(express.static(path.join(__dirname, "../")));
+
+app.get("/", (req, res) => res.sendFile("index.html"));
 
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function() {
